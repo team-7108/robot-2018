4305d88ec038354a94259058a4751a56a67c8559
Ahmet Ensar Meral <aho.dombik5258@gmail.com>
2018-08-30 11:31:19 +0300

    Added AutonomousLift classes and AutonomousLeft CommandGroup.

--- src/org/usfirst/frc7108/Robot/commands/AutonomousLift.java
+++ src/org/usfirst/frc7108/Robot/commands/AutonomousLift.java
@@ -3,48 +3,40 @@ package org.usfirst.frc7108.Robot.commands;
 import org.usfirst.frc7108.Robot.Robot;
 import org.usfirst.frc7108.Robot.RobotMap;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
-import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Command;
 
-
-public class AutonomousDrive extends Command{
-
+public class AutonomousLift extends Command{
 	long startTime;
 	double seconds;
-	double mesafe;
-	boolean durum;
 	
-	public AutonomousDrive(double _mesafe) {
-		this.mesafe = _mesafe;
-		//this.seconds = _seconds;		
-		requires(Robot.sase);
+	public AutonomousLift(double _seconds) {
+		requires(Robot.clifter);
+		this.seconds = _seconds;
 	}
 	protected void initialize() {
-		startTime = System.currentTimeMillis();
+		
 
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.sase.otonomDuz();
     	
-    	
+    	RobotMap.lifterMotor1.set(0.5);
+    	RobotMap.lifterMotor2.set(0.5);
      // may need to call getJoystick() method
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-		//return (System.currentTimeMillis() - startTime) > (int) (1000d * this.seconds);
+		return (System.currentTimeMillis() - startTime) > (int) (1000d * this.seconds);
     	
-    	return (Robot.sonAV > (int) this.mesafe);
+    	
     }
 
     // Called once after isFinished returns true
     protected void end() 
     {
-    	Robot.sase.otonomDur();
+    	Robot.sase.clifterDur();
 
     }
 
@@ -53,5 +45,9 @@ public class AutonomousDrive extends Command{
     protected void interrupted() {
     	end();
     }
+	}
 
-}
+
+
+
+
--- src/org/usfirst/frc7108/Robot/commands/AutonomousLiftLS.java
+++ src/org/usfirst/frc7108/Robot/commands/AutonomousLiftLS.java
@@ -3,48 +3,34 @@ package org.usfirst.frc7108.Robot.commands;
 import org.usfirst.frc7108.Robot.Robot;
 import org.usfirst.frc7108.Robot.RobotMap;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
-import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Command;
 
-
-public class AutonomousDrive extends Command{
-
-	long startTime;
-	double seconds;
-	double mesafe;
-	boolean durum;
-	
-	public AutonomousDrive(double _mesafe) {
-		this.mesafe = _mesafe;
-		//this.seconds = _seconds;		
-		requires(Robot.sase);
+public class AutonomousLiftLS extends Command{
+	public AutonomousLiftLS() {
+		requires(Robot.clifter);
 	}
-	protected void initialize() {
-		startTime = System.currentTimeMillis();
-
-    }
+    
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.sase.otonomDuz();
     	
-    	
+    	RobotMap.lifterMotor1.set(0.5);
+    	RobotMap.lifterMotor2.set(0.5);
      // may need to call getJoystick() method
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-		//return (System.currentTimeMillis() - startTime) > (int) (1000d * this.seconds);
+		return (Robot.counter.get()!=0);
     	
-    	return (Robot.sonAV > (int) this.mesafe);
+    	
     }
 
     // Called once after isFinished returns true
     protected void end() 
     {
-    	Robot.sase.otonomDur();
+    	Robot.sase.clifterDur();
+    	Robot.counter.reset();
 
     }
 
@@ -53,5 +39,7 @@ public class AutonomousDrive extends Command{
     protected void interrupted() {
     	end();
     }
+	
+	}
 
-}
+
