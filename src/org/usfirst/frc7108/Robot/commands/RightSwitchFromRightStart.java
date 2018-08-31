c976dbf5aab2509a587b8045a0b04af745f22705
Unknown <furkanarslan3469@gmail.com>
2018-08-31 11:44:30 +0300

    Added fms controlled autonomous.

---
+++ src/org/usfirst/frc7108/Robot/commands/RightSwitchFromRightStart.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc7108.Robot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class RightSwitchFromRightStart extends CommandGroup {
+	
+	public RightSwitchFromRightStart()
+	{
+	this.addSequential(new AutonomusDrive(350));
+ 	this.addSequential(new AutonomousDelay(0.1));
+ 	this.addSequential(new AutonomousTurnPID(90));
+ 	this.addSequential(new AutonomousDelay(0.1));
+ 	this.addSequential(new AutonomusDrive(200));
+ 	this.addSequential(new AutonomousDelay(0.1));
+ 	this.addSequential(new AutonomousTurnPID(0));
+ 	this.addSequential(new AutonomousDelay(0.1));
+ 	this.addSequential(new AutonomusDrive(200));
+ 	this.addSequential(new AutonomousDelay(0.1));
+ 	this.addSequential(new AutonomousTurnPID(-90));
+ 	this.addSequential(new AutonomousDelay(0.1));
+ 	this.addSequential(new AutonomousLift());
+ 	this.addSequential(new AutonomousDelay(0.1));
+ 	this.addSequential(new KutuBirak());
+	}
+
+}
