7b93547bbf42e5fc0681009e5e13d19a4a631975
Unknown <bicangoksel0101@gmail.com>
2018-10-02 16:39:20 +0300

    Add alliance color codes :fire:

--- src/org/usfirst/frc7108/Robot/utils/Arduino.java
+++ src/org/usfirst/frc7108/Robot/utils/Arduino.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc7108.Robot.utils;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.SerialPort;
 
@@ -7,6 +9,9 @@ public class Arduino {
 	
 	public static SerialPort arduinoUSB =  new SerialPort(9600 ,SerialPort.Port.kUSB); // USB needs to be changed
 	public static String receivedData;
+	DriverStation.Alliance Alliance = DriverStation.getInstance().getAlliance();
+	
+	
 	
 	public String readFromArduino(int receivedDataCharCount) {
 		arduinoUSB.flush();
@@ -15,7 +20,12 @@ public class Arduino {
 	}
 	
 	public void writeToArduino(String message) {
-		arduinoUSB.writeString(message);
+		
+		if (Alliance == DriverStation.Alliance.Blue) {
+			arduinoUSB.writeString("c");
+		}if (Alliance == DriverStation.Alliance.Red) {
+			arduinoUSB.writeString("a");
+		}
 	}
 
 }
